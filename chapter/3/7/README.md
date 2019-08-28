1de6cfe9993bf0ba978af86f796533af4dd23fbf
shibamoto <silverbirder@gmail.com>
2019-08-28 13:31:12 +0900

    move testing code

---
+++ chapter/3/7/README.md
@@ -0,0 +1,22 @@
+# \<my-todo>
+
+This webcomponent follows the [open-wc](https://github.com/open-wc/open-wc) recommendation.
+
+## Usage
+```html
+<script type="module">
+  import 'my-todo/my-todo.js';
+</script>
+
+<my-todo></my-todo>
+```
+
+## Testing using karma (if applied by author)
+```bash
+npm run test
+```
+
+## Testing using karma via browserstack (if applied by author)
+```bash
+npm run test:bs
+```
---
+++ chapter/3/7/test/todo-input.test.js
@@ -0,0 +1,29 @@
+import {html, fixture, oneEvent} from '@open-wc/testing-helpers';
+import {expect} from '@open-wc/testing';
+import '../my-todo.js';
+
+const parser = new DOMParser();
+
+describe('TodoInput Basic Test', () => {
+  it('contains form', async () => {
+    const el = await fixture(html`
+      <todo-input></todo-input>
+    `);
+    let shadowHTML = el.shadowRoot.innerHTML;
+    let doc = parser.parseFromString(shadowHTML, 'text/html');
+    let formID = doc.querySelector('form').getAttribute('id');
+    expect(formID).to.be.equal('new-todo-form');
+  });
+  it('dispatch submit event', async () => {
+    const el = await fixture(html`
+      <todo-input></todo-input>
+    `);
+    const inputValue = 'my todo 1';
+    const form = el.shadowRoot.getElementById('new-todo-form');
+    const input = el.shadowRoot.getElementById('new-todo');
+    input.setAttribute('value', inputValue);
+    setTimeout(() => form.dispatchEvent(new Event('submit')));
+    const {detail} = await oneEvent(el, 'onSubmit');
+    expect(detail).to.be.equal(inputValue);
+  });
+});
